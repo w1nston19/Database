826d3b74aa97ec8217e2821b3ed94e4ffb2d37ce
w1nston19 <74965282+w1nston19@users.noreply.github.com>
2022-07-16 16:05:27 +0300

    Update project_views.sql

--- project_views.sql
+++ project_views.sql
@@ -1,12 +1,12 @@
 /*View to connect part with partition order and get the total price of each part*/
-/*CREATE VIEW V_PARTITION_ORDER_MAP
+CREATE VIEW V_PARTITION_ORDER_MAP
 AS
 SELECT P.PART_ID, P.NAME, P.TOTAL_PRICE, PO.ORDER_ID, PO.message
 	FROM (SELECT ID AS PART_ID, NAME, price + replacement_price AS TOTAL_PRICE FROM PARTS ) P, 
 	     partition_order_parts_mapping M, 
 		 (SELECT ID AS ORDER_ID, MESSAGE FROM partition_order) PO
 	WHERE P.PART_ID = M.part_id AND M.order_id = PO.ORDER_ID
-	*/
+	
 
 /*View to easily get the price for specific order .*/
 CREATE VIEW V_CALCULATE_TOTAL_PRICE
